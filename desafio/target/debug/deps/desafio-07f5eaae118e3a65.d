2d92508573a2e8c014bd1a8d12cb385c50d7ddfe
ThiagoBrum2006 <thiagobrum222@gmail.com>
2025-03-22 15:25:54 +0000

    commit inicial

---
+++ desafio/target/debug/deps/desafio-07f5eaae118e3a65.d
@@ -0,0 +1,5 @@
+/workspaces/desafio-rust-ponteiro/desafio/target/debug/deps/desafio-07f5eaae118e3a65: src/main.rs
+
+/workspaces/desafio-rust-ponteiro/desafio/target/debug/deps/desafio-07f5eaae118e3a65.d: src/main.rs
+
+src/main.rs:
---
+++ desafio/target/debug/deps/desafio-b593506c6b087243.d
@@ -0,0 +1,5 @@
+/workspaces/desafio-rust-ponteiro/desafio/target/debug/deps/desafio-b593506c6b087243: src/main.rs
+
+/workspaces/desafio-rust-ponteiro/desafio/target/debug/deps/desafio-b593506c6b087243.d: src/main.rs
+
+src/main.rs:
